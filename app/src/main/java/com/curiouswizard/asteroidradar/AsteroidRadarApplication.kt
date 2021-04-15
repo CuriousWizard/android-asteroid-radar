6284451a66b6281ac03bed7133158dd2f68693d3
CuriousWizard <tremmel.david@hotmail.com>
2021-04-15 22:16:09 +0200

    Using worker for daily update

---
+++ app/src/main/java/com/curiouswizard/asteroidradar/AsteroidRadarApplication.kt
@@ -0,0 +1,53 @@
+package com.curiouswizard.asteroidradar
+
+import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.curiouswizard.asteroidradar.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class AsteroidRadarApplication: Application() {
+
+    private fun delayedInit() {
+        CoroutineScope(Dispatchers.Default).launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
+        val repeatingRequest
+                = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(baseContext).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
+    override fun onCreate() {
+        super.onCreate()
+//        Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+}
